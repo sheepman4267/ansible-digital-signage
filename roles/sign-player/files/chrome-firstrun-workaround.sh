11edca52c06edc767e18333aca9533f3d06443a0
Hans Kelson <hans.kelson@bloomington.in.gov>
2022-08-29 15:27:32 -0400

    Work around Google Chrome's firstrun dialog

---
+++ roles/sign-player/files/chrome-firstrun-workaround.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This unit was created by ansible-digital-signage. Any modifications will be lost next time the playbook is run.
+# More information can be found at https://github.com/City-of-Bloomington/ansible-digital-signage
+
+export DISPLAY=:0
+xdotool key Tab
+xdotool key KP_Enter
+xdotool key Tab
+xdotool key KP_Enter
+xdotool key Tab
+xdotool key Tab
+xdotool key KP_Enter
+touch ~/chrome-has-been-run
